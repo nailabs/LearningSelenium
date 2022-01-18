c50afe4f9a208983a51c292185539f02b628f204
nailabs <absalyamovan@gmail.com>
2022-01-18 20:28:54 +0300

    Made changes

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -4,13 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
        // RemoteWebDriver driverF = new FirefoxDriver();//polymorphism from parent class
         driver.get("http://google.com");//to open
-
-
+        Thread.sleep(3000);//wait for 3 sec
+        driver.close();
     }
 }

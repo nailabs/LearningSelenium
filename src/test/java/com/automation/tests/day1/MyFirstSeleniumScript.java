ddb53fe7523dc071ce3767ffd7ad8325fc92d135
nailabs <absalyamovan@gmail.com>
2022-01-18 19:07:22 +0300

    first commit today

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumScript {
+    public static void main(String[] args) {
+
+//setup chromedriver
+        WebDriverManager.chromedriver().setup();
+
+        //create chromedriver object
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+
+
+
+    }
+}
